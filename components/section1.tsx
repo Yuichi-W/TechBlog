edf3dc2f4b50a58e629cde6acf508c1cf6c655ef
y.wada <wada1257@gmail.com>
2023-03-15 00:02:58 +0900

    cerate section

--- components/section1.tsx
+++ components/section1.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Author } from './_child/author'
+import Image from "next/image";
+import Link from "next/link";
+import { Author } from './_child/author';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/css';
+
+const images = [
+    '/images/img1.jpg',
+    '/images/img2.jpg',
+    '/images/img3.png',
+    '/images/articles/img1.jpg',
+    '/images/articles/img2.jpg',
+];
 
 export const Section1 = () => {
-
+    SwiperCore.use([Autoplay]);
     const bg = {
         backgroundImage: "url('/images/banner.png')",
         backgroundRepeat: "no-repeat",
@@ -14,18 +26,35 @@ export const Section1 = () => {
         <section className="py-16" style={bg}>
             <div className="container mx-auto md:px-20">
                 <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
-                { Slide() }
+                <Swiper
+                    slidesPerView={1}
+                    loop={true}
+                    autoplay={{
+                        delay:2000
+                    }}
+                >
+                {images.map((src, index) => (
+                    <SwiperSlide key={index}>
+                        <Slide src={src} />
+                    </SwiperSlide>
+                ))}
+                </Swiper>
+                
             </div>
         </section>
     );
 }
 
-function Slide(){
+type SlideProps = {
+    src: string;
+}
+
+function Slide({ src }: SlideProps){
     return (
         <div className="grid md:grid-cols-2">
             <div className="image">
                 <Link href={"/"}>
-                    <Image src="/images/pc_img.jpg" width={640} height={853} alt="PC Image" />
+                    <Image src={ src } width={640} height={853} alt="PC Image" />
                 </Link>
             </div>
             <div className="info flex justify-center flex-col">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import { Format } from '../layout/format'
 import { Section1 } from '../components/section1'
+import { Section2 } from '../components/section2'
+import { Section3 } from '../components/section3'
 
 export default function Home() {
   return (
     <Format>
       <Section1 />
+      <Section2 />
+      <Section3 />
     </Format>
   )
 }
