c4b858c3f57129e4af25f3148a37c072274310d2
y.wada <wada1257@gmail.com>
2023-03-19 23:46:51 +0900

    fix

--- components/latest.tsx
+++ components/latest.tsx
@@ -15,9 +15,9 @@ export const Latest = () => {
         <section className="container mx-auto md:px-20 py-10">
             <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-            {data ? data.map((post: BlogPost, index: number) => (
+            {data && data.map((post: BlogPost, index: number) => (
                 <Post key={index} postData={post} />
-            )) : null}
+            ))}
             </div>
         </section>
     )
--- lib/fetcher.tsx
+++ lib/fetcher.tsx
@@ -1,32 +1,31 @@
-import useSWR from 'swr';
 import { useEffect, useState } from 'react';
 
 const baseURL = "http://localhost:3000/";
 
 export const Fetcher = ({ endpoint }: { endpoint: string }) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${baseURL}${endpoint}`);
-        const json = await response.json();
-        setData(json);
-        setIsLoading(false);
-      } catch (error) {
-        setIsError(true);
-        setIsLoading(false);
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const response = await fetch(`${baseURL}${endpoint}`);
+            const json = await response.json();
+            setData(json);
+            setIsLoading(false);
+        } catch (error) {
+            setIsError(true);
+            setIsLoading(false);
+        }
+        };
+
+        fetchData();
+    }, [endpoint]);
+
+    return {
+        data,
+        isLoading,
+        isError,
     };
-
-    fetchData();
-  }, [endpoint]);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
 };
